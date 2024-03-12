0e33e6b62243367fb5691674485bba4929d4cbee
Kanbi Dungaitj <openkagi@outlook.com>
2024-03-12 08:01:11 +0800

    Patch 1.10 Update

--- main.py
+++ main.py
@@ -1,55 +1,40 @@
 def tc_slashn(text: str, length: int):
-    marks_1 = ("，", "。", "、", "！", "？","；", "：", "」", "》", "～", "—")
-    marks_2 = ("「", "《")
+    marks = ("，", "。", "、", "！", "？","；", "：", "」", "》", "～", "—", "「", "《")
 
-    length_counter = 0
-    is_english = True
-    result = ""    
+    length_counter = [0, 0]
+    result = ""
 
     for character in text:
-        if ord(character) > 127:
-            length_counter += 2
-            is_english = False
-        else:
-            length_counter += 1
+        length_counter[1] = 0
+        is_english = (not (ord(character) > 127)) and (character != " ")
 
-            if character != " ":
-                is_english = True
-            else:
-                is_english = False
-        
-        if character in marks_1 or character in marks_2:
-            tmp_result = result.split("\n")
+        length_counter[0] += 2
+        if ord(character) <= 127:
+            length_counter[0] -= 1
 
-            if length_counter == 2 and character not in marks_2:
-                tmp_result[-2] += character
-                tmp_result.pop(-1)
-
-                result = ""
-
-                for l in tmp_result:
-                    result += l + "\n"
-
-                length_counter = 0
-            elif length_counter >= length and character not in marks_2:
-                result += character + "\n"
-                length_counter = 0
-            elif length_counter == length and character in marks_2:
-                result += "\n" + character
-                length_counter = 0
+        if character in marks:
+            if (length_counter[0] == 2) and (character not in marks[-2:]):
+                result = result[:-1]
+                result += character + "⠀"
+            elif (length_counter[0] >= length) and (character not in marks[-2:]):
+                result += character + "⠀"
+            elif (length_counter[0] == length) and (character in marks[-2:]):
+                result += "⠀" + character
             else:
                 result += character
-        elif length_counter >= length and not is_english:
-            if character == " ":
-                result += "\n"
-            else:
-                result += character + "\n"
+                length_counter[1] = length_counter[0]
 
-            length_counter = 0
+            length_counter[0] = length_counter[1]
+        elif (length_counter[0] >= length) and (not is_english):
+            result += character.replace(" ", "") + "⠀"
+            length_counter[0] = length_counter[1]
         else:
             result += character
-    
-    return result
+
+    return result.replace("⠀", "\n")
 
 if __name__ == "__main__":
-    print(tc_slashn("在 5 分鐘之內根據主題築起一個建築！透過「傳奇屎詩」到「傳奇」的評級進行各建築的評分。目標是在 16 名玩家中榮獲至多的投票！", 20))
+    text = "在 5 分鐘之內根據主題築起一個建築！透過「傳奇屎詩」到「傳奇」的評級進行各建築的評分。目標是在 16 名玩家中榮獲至多的投票！"
+    length = 20
+
+    print(tc_slashn(text, length))
